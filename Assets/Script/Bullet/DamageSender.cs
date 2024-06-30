fd04fdfe06ffa4105a0d0ff6945f540d6318a831
leemindark2709 <thanhductc27@gmail.com>
2024-06-30 23:20:22 +0700

    xoa map

--- Assets/Script/Bullet/DamageSender.cs
+++ Assets/Script/Bullet/DamageSender.cs
@@ -1,41 +1,49 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class DamageSender : MonoBehaviour
-{   
+{
     public float damage = 1;
-    [SerializeField] private AudioManager audioManger; // Biến để lưu trữ AudioSource
+    [SerializeField] private AudioManager audioManager; // Biến để lưu trữ AudioSource
 
     private void Awake()
     {
         // Gán AudioSource component
-        audioManger = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageReceiver damageReceiver = other.GetComponent<DamageReceiver>();
         if (damageReceiver != null)
         {
-            audioManger.PlaySFX(audioManger.DameSender);
+            audioManager.PlaySFX(audioManager.DameSender);
             damageReceiver.Damaged(this.damage);
             BulletManager.instance.SpawnExplosion("ExplosionBullet", other.transform.position);
         }
 
         // Kiểm tra xem đối tượng va chạm có phải là Tilemap hay không
-        //Tilemap tilemap = other.GetComponent<Tilemap>();
-        //if (tilemap != null)
-        //{
-        //    // Xử lý va chạm với Tilemap
-        //    Vector3 hitPosition = other.transform.position;
-        //    Vector3Int tilePosition = tilemap.WorldToCell(hitPosition);
-        //    if (tilemap.HasTile(tilePosition))
-        //    {
-        //        Debug.Log("Tile collided at: " + tilePosition);
-        //        tilemap.SetTile(tilePosition, null);
-        //    }
-        //}
+        Tilemap tilemap = other.GetComponent<Tilemap>();
+        if (tilemap != null)
+        {
+            // Xử lý va chạm với Tilemap
+            Vector3 hitPosition = other.ClosestPoint(transform.position);
+            Vector3Int tilePosition = tilemap.WorldToCell(hitPosition);
+            if (tilemap.HasTile(tilePosition))
+            {
+                Debug.Log("Tile collided at: " + tilePosition);
+                tilemap.SetTile(tilePosition, null);
+            }
+        }
+
+        // Kiểm tra xem đối tượng va chạm có tên là "map" hay không
+        if (other.gameObject.name == "map")
+        {
+            Debug.Log("Collided with map object: " + other.gameObject.name);
+            Destroy(other.gameObject); // Xóa đối tượng map
+        }
 
         this.Despawn();
     }
